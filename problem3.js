9547b2fc2a57de8c37c00dfa109506831fe51e2f
himel2535 <monwarhossanhimel@gmail.com>
2025-08-13 23:54:04 +0600

    from input object properties value check and condition which team is fair in football-problem3

---
+++ problem3.js
@@ -0,0 +1,26 @@
+const player1 = { name: "Brazil", foul: 5, cardY: 1, cardR: 0 };
+const player2 = { name: "Argentina", foul: 7, cardY: 0, cardR: 0};
+
+function bestTeam(player1, player2) {
+  player1UnFair = player1.foul + player1.cardY + player1.cardR;
+  player2UnFair = player2.foul + player2.cardY + player2.cardR;
+
+  if (
+    typeof player1 !== "object" ||
+    typeof player2 !== "object" ||
+    Array.isArray(player1) ||
+    Array.isArray(player2)
+  ) {
+    return "Invalid";
+  } else {
+    if (player1UnFair < player2UnFair) {
+      return player1.name;
+    } else if (player2UnFair < player1UnFair) {
+      return player2.name;
+    } else {
+      return "Tie";
+    }
+  }
+}
+result = bestTeam(player1, player2);
+console.log(result);
