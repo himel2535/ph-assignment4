fd4189b3c11f341f2e0d10ba530a5aed8de48a2c
himel2535 <monwarhossanhimel@gmail.com>
2025-08-14 00:16:23 +0600

    after add const in variable for silly mistak in problem3

--- problem3.js
+++ problem3.js
@@ -1,24 +1,29 @@
 const player1 = { name: "Brazil", foul: 5, cardY: 1, cardR: 0 };
-const player2 = { name: "Argentina", foul: 7, cardY: 0, cardR: 0};
+const player2 = { name: "Argentina", foul: 7, cardY: 0, cardR: 0 };
 
 function bestTeam(player1, player2) {
-  player1UnFair = player1.foul + player1.cardY + player1.cardR;
-  player2UnFair = player2.foul + player2.cardY + player2.cardR;
+    const player1UnFair = player1.foul + player1.cardY + player1.cardR;
+    const player2UnFair = player2.foul + player2.cardY + player2.cardR;
 
-  if (
+    if (
     typeof player1 !== "object" ||
     typeof player2 !== "object" ||
     Array.isArray(player1) ||
     Array.isArray(player2)
-  ) {
-    return "Invalid";
-  } else {
+    ) 
+    {
+        return "Invalid";
+    }
+
+    else {
     if (player1UnFair < player2UnFair) {
-      return player1.name;
-    } else if (player2UnFair < player1UnFair) {
-      return player2.name;
-    } else {
-      return "Tie";
+        return player1.name;
+    } 
+    else if (player2UnFair < player1UnFair) {
+        return player2.name;
+    } 
+    else {
+        return "Tie";
     }
   }
 }
