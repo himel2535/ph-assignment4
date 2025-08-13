0eae281a46801adf2231d9bd9bab23e9cb0609fa
himel2535 <monwarhossanhimel@gmail.com>
2025-08-14 00:19:25 +0600

    after adding let or const all value assign bcz of it will be global

--- problem1.js
+++ problem1.js
@@ -3,7 +3,7 @@ function totalFine(fare) {
     if(fare<=0 || typeof fare!=="number"){
         return "Invalid"  ;  
     }
-    fine=fare+fare*(20/100)+30;
+    let fine=fare+fare*(20/100)+30;
     return fine;
 }
 result=totalFine(fare);
--- problem2.js
+++ problem2.js
@@ -3,7 +3,7 @@ function onlyCharacter(str) {
   if (typeof str !== "string") {
     return "Invalid";
   }
-  final = str.split(" ").join("").toUpperCase();
+  let final = str.split(" ").join("").toUpperCase();
   return final;
 }
 result = onlyCharacter(str);
